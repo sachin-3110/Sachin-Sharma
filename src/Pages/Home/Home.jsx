5970ea379c85210dd95b8dfd1716c5fcedd1d1b9
Sachin <sachinsharma8005@gmail.com>
2025-11-06 15:30:16 +0530

    cursor activated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,11 +3,18 @@ import TopNav from "../../components/TopNav/TopNav";
 import ScrollToTop from "../../components/ScrollToTop";
 import { Outlet } from "react-router-dom";
 import LowerNav from "../../components/LowerNav";
+import ThemeButton from "../../components/ThemeButton";
+import CustomCursor from "../../components/CustomCursor";
+
 const Container = () => {
 
   return (
       <div className="min-w-full border-yellow-500 relative md:p-0 z-0 min-h-screen flex flex-col items-center mx-auto">
         {/* <BackgroundAnimation/> */}
+        <div className="absolute z-5"> 
+          <ThemeButton/>
+        </div>
+        <CustomCursor/>
         <TopNav />
         <ScrollToTop/>
         <Outlet  />
--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import ThemeButton from "../ThemeButton";
-import { useLocation } from "react-router-dom";
 import HeroSection from "./HeroSection";
 import { motion } from "motion/react";
 const TopNav = () => {
@@ -8,8 +6,8 @@ const TopNav = () => {
   const [Time, setTime] = useState([]);
   useEffect(() => {
     const updateDateTime = () => {
-      var onlyDate = new Date().toLocaleString().split("/");
-      var onlyTime = new Intl.DateTimeFormat("en-GB", {
+      let onlyDate = new Date().toLocaleString().split("/");
+      let onlyTime = new Intl.DateTimeFormat("en-GB", {
         hour: "2-digit",
         minute: "2-digit",
         second: "2-digit",
@@ -30,21 +28,19 @@ const TopNav = () => {
       transition={{ duration: 0.5 }}
       className="w-[90%] bg-black m-2  flex font-[solenoid] font-bold justify-center sm:justify-between sm:px-3 border-[1px] flex-col border-white rounded-2xl min-h-[22vh] md:flex-row md:min-h-[10vh] md:my-5"
     >
-      <h1 className="uppercase font-extrabold flex justify-center items-center text-3xl">
-        {/* {displayName} */}
+      <h1 className="normal tracking-wider font-extrabold flex justify-center items-center text-5xl">
         <HeroSection />
       </h1>
-      <ThemeButton />
 
-      <div id="time" className="p-3 flex flex-col justify-center items-center">
-        <div className="sm:text-2xl flex md:gap-1">
+      <div id="time" className="p-3 flex justify-evenly items-center">
+        <div className="text-2xl flex md:gap-1">
           <h1>{date[0]}/</h1>
           <h1>{date[1]}/</h1>
           <h1>{date[2]?.split(",")[0]}</h1>
         </div>
-        <div className="text-sm sm:text-2xl flex gap-1 font-bold text-yellow-200">
+        <div className="text-2xl min-w-40 border-2 justify-center rounded flex gap-1 font-bold text-yellow-200">
           <h1 className="">{Time[0]}</h1>:<h1 className="">{Time[1]}</h1>:
-          <h1 className=" duration-200 animate-bounce text-2xl">
+          <h1 className=" duration-200 animate-none">
             {Time[2]?.split("").slice(0, 2).join("")}
           </h1>
           <h1 className="uppercase duration-200">
