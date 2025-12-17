f9187cc739f49d77edd394eaf141433b81e42ee7
Sachin <sachinsharma8005@gmail.com>
2025-12-17 23:05:26 +0530

    Under maintenence component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Home from './Pages/Home/Home.jsx'
+import Maintanence from './components/Maintanence.jsx'
 
 const App = () => {
   return (
     <div className='min-h-screen no-scrollbar w-full bg-black text-white'>
-      <Home/>
-
+      <Maintanence/>
     </div>
   )
 }
---
+++ src/components/Maintanence.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Maintanence = () => {
+  return (
+    <div className='w-full h-screen text-2xl flex flex-col justify-center items-center'>
+        <div id="gif" className='h-[50vh]'>
+            <img src="https://media.tenor.com/K5wSW-CGK9wAAAAj/maintenance-under-maintenance.gif" className='h-full w-full'></img>    
+        </div>  
+    </div>
+  )
+}
+
+export default Maintanence
