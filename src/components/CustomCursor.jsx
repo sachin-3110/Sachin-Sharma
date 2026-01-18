b0436595ae1f9adc8e6fc59ef01481a7a154c089
Sachin <sachinsharma8005@gmail.com>
2026-01-19 00:59:44 +0530

    framer-motion removed from customcursor

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -1,4 +1,4 @@
-import { motion, useMotionValue, useSpring } from "framer-motion";
+import { motion, useMotionValue, useSpring } from "motion/react";
 import React, { useEffect } from "react";
 
 const CustomCursor = () => {
