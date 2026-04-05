e1f4ba93bdeb590f5ce48791f2c72cd2ff9c960d
Sachin <sachinsharma8005@gmail.com>
2026-04-05 22:36:19 +0530

    navbar fixed

--- src/components/LowerNav.jsx
+++ src/components/LowerNav.jsx
@@ -18,25 +18,22 @@ const LowerNav = () => {
     },
   ]);
   return (
-    <div className="flex bg-white/20 w-full py-2 backdrop-blur-sm justify-evenly text-white text-2xl md:text-3xl items-center h-full rounded-2xl">
+    <div className="flex bg-white/20 w-full py-2 backdrop-blur-sm justify-evenly text-white text-2xl md:text-3xl items-center h-full rounded-xl">
       {icons?.map((elem, index) => (
-        <div
+        <NavLink
+          to={elem.path}
           key={index + 1}
-          className="group h-full flex justify-center items-center"
+          className="group flex flex-col justify-center items-center"
         >
-          <NavLink
-            to={`${elem.path}`}
-            className={({ isActive }) =>
-              `${
-                isActive
-                  ? "text-yellow-200 text-2xl scale-125 duration-400"
-                  : "text-white text-2xl"
-              } ${
-                elem.iconCode
-              } hover:scale-120 w-full duration-200 h-full flex justify-center items-center`
-            }
-          ></NavLink>
-        </div>
+        {({isActive})=> <>
+          <i className={`${elem.iconCode}   ${isActive ? "text-yellow-200":""}`}></i>
+          <div
+            id="circle"
+            className={`${isActive ? "bg-yellow-200 w-2 group-hover:bg-yellow-200 " : ""}group-hover:w-2  group-hover:bg-white duration-200 rounded-full h-2 w-0 bg-transparent `}
+          >           
+          </div>
+        </>}
+        </NavLink>
       ))}
     </div>
   );
